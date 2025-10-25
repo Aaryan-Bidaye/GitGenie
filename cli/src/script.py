99372babe77284fe4a72270bdfb5d99d3f993952
Aaryan-Bidaye <94824629+Aaryan-Bidaye@users.noreply.github.com>
2025-10-25 02:04:14 -0700

    created cli base

--- cli/src/script.py
+++ cli/src/script.py
@@ -1,15 +1,22 @@
 import subprocess
 #documentation here https://llm.datasette.io/en/stable/
 import llm
-
+import typer
 #model = llm.get_model("gpt-4o-mini")
 #rint(llm.get_model().prompt("Five surprising names for a pet pelican"))
 
-print(subprocess.run("git diff --cached > eaklfsjladf.txt", shell=True))
-with open("eaklfsjladf.txt", "r") as f:
-    data = f.read()
-print(data)
-print(subprocess.run("rm eaklfsjladf.txt", shell=True))
+app = typer.Typer()
+
+@app.command()
+def main():
+    print(subprocess.run("git diff --cached > change_material.txt", shell=True))
+    with open("change_material.txt", "r") as f:
+        data = f.read()
+    print(data)
+    print(subprocess.run("rm change_material.txt", shell=True))
+
+if __name__ == "__main__":
+    app()
 
 
 
